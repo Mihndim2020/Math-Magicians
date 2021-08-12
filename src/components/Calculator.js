87d7d89c4ae83a8980010f0b46081108ed1d0405
Mihndim Julius <mihndim2016@gmail.com>
2021-08-12 17:50:05 -0400

    Convert class component to a function component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,64 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: '',
-      next: '',
-      operation: '',
-    };
-    this.onClickHandler = this.onClickHandler.bind(this);
-  }
+// class Calculator extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       total: '',
+//       next: '',
+//       operation: '',
+//     };
+//     this.onClickHandler = this.onClickHandler.bind(this);
+//   }
 
-  onClickHandler(newValue) {
+const Calculator = () => {
+  // const setFunctions = {setTotal, setNext, setOperation};
+  const [{ total, next, operation }, { setTotal, setNext, setOperation }] = useState('');
+  const obj = { total, next, operation };
+
+  const onClickHandler = (newValue) => {
     if (newValue.total != null) {
-      this.setState({ total: newValue.total });
+      setTotal(newValue.total);
     }
 
-    this.setState({ next: newValue.next });
+    setNext(newValue.next);
 
     if (newValue.operation != null) {
-      this.setState({ operation: newValue.operation });
+      setOperation(newValue.operation);
     }
-  }
+  };
 
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <div className="calculator-container">
-        <div className="display-area">
-          <p className="display-results">
-            { total }
-            { operation }
-            { next }
-          </p>
-        </div>
-        <div className="buttons-container">
-          <button type="button" className="calculator-button" onClick={() => { this.onClickHandler(calculate(this.state, 'AC')); }}>AC</button>
-          <button type="button" className="calculator-button" onClick={() => { this.onClickHandler(calculate(this.state, '+/-')); }}>+/-</button>
-          <button type="button" className="calculator-button" onClick={() => { this.onClickHandler(calculate(this.state, '%')); }}>%</button>
-          <button type="button" className="operator-button" onClick={() => { this.onClickHandler(calculate(this.state, '÷')); }}>÷</button>
-          <button type="button" className="calculator-button" onClick={() => { this.onClickHandler(calculate(this.state, '7')); }}>7</button>
-          <button type="button" className="calculator-button" onClick={() => { this.onClickHandler(calculate(this.state, '8')); }}>8</button>
-          <button type="button" className="calculator-button" onClick={() => { this.onClickHandler(calculate(this.state, '9')); }}>9</button>
-          <button type="button" className="operator-button" onClick={() => { this.onClickHandler(calculate(this.state, 'x')); }}>x</button>
-          <button type="button" className="calculator-button" onClick={() => { this.onClickHandler(calculate(this.state, '4')); }}>4</button>
-          <button type="button" className="calculator-button" onClick={() => { this.onClickHandler(calculate(this.state, '5')); }}>5</button>
-          <button type="button" className="calculator-button" onClick={() => { this.onClickHandler(calculate(this.state, '6')); }}>6</button>
-          <button type="button" className="operator-button" onClick={() => { this.onClickHandler(calculate(this.state, '-')); }}>-</button>
-          <button type="button" className="calculator-button" onClick={() => { this.onClickHandler(calculate(this.state, '1')); }}>1</button>
-          <button type="button" className="calculator-button" onClick={() => { this.onClickHandler(calculate(this.state, '2')); }}>2</button>
-          <button type="button" className="calculator-button" onClick={() => { this.onClickHandler(calculate(this.state, '3')); }}>3</button>
-          <button type="button" className="operator-button" onClick={() => { this.onClickHandler(calculate(this.state, '+')); }}>+</button>
-          <button type="button" className="calculator-button zero" onClick={() => { this.onClickHandler(calculate(this.state, '0')); }}>0</button>
-          <button type="button" className="calculator-button" onClick={() => { this.onClickHandler(calculate(this.state, '.')); }}>.</button>
-          <button type="button" className="operator-button" onClick={() => { this.onClickHandler(calculate(this.state, '=')); }}>=</button>
-        </div>
+  return (
+    <div className="calculator-container">
+      <div className="display-area">
+        <p className="display-results">
+          { total }
+          { operation }
+          { next }
+        </p>
       </div>
-    );
-  }
-}
+      <div className="buttons-container">
+        <button type="button" className="calculator-button" onClick={() => { onClickHandler(calculate(obj, 'AC')); }}>AC</button>
+        <button type="button" className="calculator-button" onClick={() => { onClickHandler(calculate(obj, '+/-')); }}>+/-</button>
+        <button type="button" className="calculator-button" onClick={() => { onClickHandler(calculate(obj, '%')); }}>%</button>
+        <button type="button" className="operator-button" onClick={() => { onClickHandler(calculate(obj, '÷')); }}>÷</button>
+        <button type="button" className="calculator-button" onClick={() => { onClickHandler(calculate(obj, '7')); }}>7</button>
+        <button type="button" className="calculator-button" onClick={() => { onClickHandler(calculate(obj, '8')); }}>8</button>
+        <button type="button" className="calculator-button" onClick={() => { onClickHandler(calculate(obj, '9')); }}>9</button>
+        <button type="button" className="operator-button" onClick={() => { onClickHandler(calculate(obj, 'x')); }}>x</button>
+        <button type="button" className="calculator-button" onClick={() => { onClickHandler(calculate(obj, '4')); }}>4</button>
+        <button type="button" className="calculator-button" onClick={() => { onClickHandler(calculate(obj, '5')); }}>5</button>
+        <button type="button" className="calculator-button" onClick={() => { onClickHandler(calculate(obj, '6')); }}>6</button>
+        <button type="button" className="operator-button" onClick={() => { onClickHandler(calculate(obj, '-')); }}>-</button>
+        <button type="button" className="calculator-button" onClick={() => { onClickHandler(calculate(obj, '1')); }}>1</button>
+        <button type="button" className="calculator-button" onClick={() => { onClickHandler(calculate(obj, '2')); }}>2</button>
+        <button type="button" className="calculator-button" onClick={() => { onClickHandler(calculate(obj, '3')); }}>3</button>
+        <button type="button" className="operator-button" onClick={() => { onClickHandler(calculate(obj, '+')); }}>+</button>
+        <button type="button" className="calculator-button zero" onClick={() => { onClickHandler(calculate(obj, '0')); }}>0</button>
+        <button type="button" className="calculator-button" onClick={() => { onClickHandler(calculate(obj, '.')); }}>.</button>
+        <button type="button" className="operator-button" onClick={() => { onClickHandler(calculate(obj, '=')); }}>=</button>
+      </div>
+    </div>
+  );
+};
 
 export default Calculator;
